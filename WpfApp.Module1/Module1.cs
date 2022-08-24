c48454f6c22083803446f095b7e2c8ccf0f91c4f
Dylan Kentish <dylankentish@live.co.uk>
2022-08-24 14:23:40 +0100

    Added Module1 and Infrastructure projects

---
+++ WpfApp.Module1/Module1.cs
@@ -0,0 +1,18 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+
+namespace WpfApp.Module1
+{
+    public class Module1 : IModule
+    {
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
