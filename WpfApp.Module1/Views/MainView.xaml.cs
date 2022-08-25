8e242bd8deb76e70abfc72269b747ede1b324761
Dylan Kentish <dylankentish@live.co.uk>
2022-08-25 09:39:46 +0100

    Updated Module1 to have a demo view

---
+++ WpfApp.Module1/Views/MainView.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace WpfApp.Module1.Views
+{
+    /// <summary>
+    /// Interaction logic for MainView.xaml
+    /// </summary>
+    public partial class MainView
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
