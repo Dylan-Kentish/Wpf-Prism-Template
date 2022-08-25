b5a4fd5e4ae549e47482b886024632288a964246
Dylan Kentish <dylankentish@live.co.uk>
2022-08-25 09:39:46 +0100

    Updated Module1 to have a demo view

--- WpfApp.Module1/Module1.cs
+++ WpfApp.Module1/Module1.cs
@@ -1,6 +1,10 @@
 ﻿using Prism.Ioc;
 using Prism.Modularity;
+using Prism.Regions;
 using Serilog;
+using WpfApp.Infrastructure;
+using WpfApp.Module1.ViewModels;
+using WpfApp.Module1.Views;
 
 namespace WpfApp.Module1
 {
@@ -8,11 +12,16 @@ namespace WpfApp.Module1
     {
         public void RegisterTypes(IContainerRegistry containerRegistry)
         {
+            containerRegistry.RegisterForNavigation<MainView, MainViewVM>();
         }
 
         public void OnInitialized(IContainerProvider containerProvider)
         {
             containerProvider.Resolve<ILogger>().Information("Module 1 Initialized");
+
+            var regionManager = containerProvider.Resolve<IRegionManager>();
+
+            regionManager.RequestNavigate(RegionNames.Root, nameof(MainView));
         }
     }
 }
---
+++ WpfApp.Module1/ViewModels/MainViewVM.cs
@@ -0,0 +1,20 @@
+﻿using Prism.Mvvm;
+
+namespace WpfApp.Module1.ViewModels
+{
+    public class MainViewVM : BindableBase
+    {
+        private string _message;
+
+        public MainViewVM()
+        {
+            Message = "I am the Main View from Module 1.";
+        }
+
+        public string Message
+        {
+            get => _message;
+            set => SetProperty(ref _message, value);
+        }
+    }
+}
